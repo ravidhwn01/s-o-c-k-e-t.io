b1207f186f4a87d7d62083199a5c87d1edcf7b82
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-09 01:24:22 +0530

    complete socket.io

--- frontend/baatain/src/App.js
+++ frontend/baatain/src/App.js
@@ -1,37 +1,57 @@
-import { useState } from "react";
-import './App.css';
-import { nanoid } from 'nanoid'
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 import { io } from "socket.io-client";
-
+const userName = nanoid(4)
 // no dotenv
 const socket = io.connect("http://localhost:5000");
 
-
 function App() {
-  const [messages, setMessages] = useState('');
+  const [messages, setMessages] = useState("");
   const [chat, setChat] = useState([]);
 
-// sending a text
-const sendChat = (e)=>{
-  e.preventDefault();
-  socket.emit('chat', {messages});
-  setMessages('');
-}
+  // sending a text
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { messages , userName});
 
+    setMessages("");
+  };
+
+  useEffect(() => {
+    // listening
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
 
   return (
     <div className="App">
       <header className="App-header">
-       <h1>baatain</h1>
-       <form onSubmit={sendChat} >
-          <input type="text" value={messages} onChange={(e) => setMessages(e.target.value)} />
-          <button type="submit" onClick={(e) => {
-            e.preventDefault();
-            socket.emit('chat', messages);
-            setMessages('');
-          }}>Send</button>
-
-       </form>
+        <h1>Adhoori Baatain</h1>
+        {chat.map((payload, index) => {
+          return (<p key={index}>{payload}  <span> user: {userName}</span> </p>)
+        })}
+       
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+          name="chat"
+            value={messages}
+            placeholder="send message"
+            onChange={(e) => setMessages(e.target.value)}
+          />
+          <button className="btn"
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log(messages);
+              socket.emit("chat", messages);
+              setMessages("");
+            }}
+          >
+            Send
+          </button>
+        </form>
       </header>
     </div>
   );
